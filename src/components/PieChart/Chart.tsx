d9d493d7266aba823bada2c66199663c3ea11dbf
RaveloMevaSoavina <mevaravelosoavina@gmail.com>
2023-08-22 15:30:47 +0300

    feat(#0010): update legend

--- src/components/PieChart/Chart.tsx
+++ src/components/PieChart/Chart.tsx
@@ -17,9 +17,15 @@ const data = {
   ],
 };
 
-const options = {
+const options: any = {
   animation: {
     duration: 0, // Set the animation duration to 0 to disable animations
+  },
+  plugins: {
+    legend: {
+      display: true,
+      position: 'center',
+    },
   },
 };
 
